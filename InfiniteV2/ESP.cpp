53f13620362266bf0858443d8d18ab40fcf213ef
ALittlePatate <maxime@patate.dev>
2023-06-19 08:45:45 +0200

    add: uncomplete ESP

---
+++ InfiniteV2/ESP.cpp
@@ -0,0 +1,20 @@
+#include "ESP.h"
+#include "Interfaces.h"
+
+void ESP::DoESP() {
+	//TODO add ESP to the config and check wether it's on or not
+
+	for (int i = 0; i < g_EntList->GetHighestEntityIndex(); i++) {
+		IEntity* ent = g_EntList->GetBaseEntity(i);
+		if (!ent) continue;
+
+		if (ent->IsBasePlayerController()) continue; //i didn't put this check in the one b4, never trust the "execution order" as seen in "The C Programming Language"
+		
+		//if (ent->m_lifeState != ALIVE) continue; //TODO
+		
+		BoundingBox box;
+		ent->GetBoundingBox(box);
+
+		//ImGui::GetBackgroundDrawList()->AddRect()
+	}
+}
